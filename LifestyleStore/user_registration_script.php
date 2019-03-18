733be7bd3842b2945fdbe6202c74fd220e4fbcb6
root <root@theonemirror.eng.vmware.com>
2019-03-18 07:41:03 +0000

    configuring connection

--- LifestyleStore/user_registration_script.php
+++ LifestyleStore/user_registration_script.php
@@ -3,9 +3,10 @@
     session_start();
     $name= mysqli_real_escape_string($con,$_POST['name']);
     $email=mysqli_real_escape_string($con,$_POST['email']);
+
     $regex_email="/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,3})$/";
     if(!preg_match($regex_email,$email)){
-        echo "Incorrect email. Redirecting you back to registration page...";
+	echo "incorrect email. Redirecting you back to registration page...";
         ?>
         <meta http-equiv="refresh" content="2;url=signup.php" />
         <?php
